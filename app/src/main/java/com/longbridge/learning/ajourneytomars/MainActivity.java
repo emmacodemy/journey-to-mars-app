b2912cf2877ec9fa37e39a8c160c04de85e5b03a
ayodele emmanuel <eayodeleof18@gmail.com>
2018-08-16 11:05:21 +0100

    new Application class that set app to portrait mode only

--- app/src/main/java/com/longbridge/learning/ajourneytomars/MainActivity.java
+++ app/src/main/java/com/longbridge/learning/ajourneytomars/MainActivity.java
@@ -2,6 +2,7 @@ package com.longbridge.learning.ajourneytomars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         titleButton = (Button) findViewById(R.id.titleButton);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        titleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEditText.getText().toString();
+                loadPage(name);
+            }
+        });
+    }
 
+    private void loadPage(String name) {
     }
 }
